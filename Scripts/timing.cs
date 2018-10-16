c09508d4716c673d48e2de9512613a2d6ec1c260
John Kim <john.j.kim@vanderbilt.edu>
2018-10-16 13:23:23 -0500

    Created scripts folder & added timing script

---
+++ Scripts/timing.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timing : MonoBehaviour {
+    float timer;
+    // button functionality commented out
+    //public Button button;
+
+    void Start() {
+        //Button b = button.GetComponent<Button>();
+        //b.onClick.AddListener(OnClick);
+        timer = 0.0f;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log(timer);
+            timer = 0.0f;
+        }
+    }
+
+    // Listener for button click
+    void OnClick() {
+        Debug.Log(timer);
+        timer = 0.0f;
+    }
+}
